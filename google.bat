38df069bf4192956e1e2fdcd8c9da4f74289dd16
osmirnov <osmirnov@arlo.com>
2023-05-08 17:26:15 +0300

    fix typo

--- google.bat
+++ google.bat
@@ -1,5 +1,5 @@
-REM THE BEST PROGRAMM I HAVE EVER WRITTEN!!!
-REM Copy-right: <a13ks3y.ass@gmail.com> https://a13ks3y.github.io
+REM THE BEST PROGRAM I HAVE EVER WRITTEN!!!
+REM Copy-right: a13ks3y.ass@gmail.com https://a13ks3y.github.io
 REM https://github.com/a13ks3y/google-cli
 @echo off
 "C:\Program Files\Google\Chrome\Application\chrome.exe" https://google.com/search?q=%1
